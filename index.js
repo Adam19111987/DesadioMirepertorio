dbcd80748b19f09de44236b72ba790c4e5f160eb
AdamDev <a.diaz.1987.08@hotmail.com>
2025-02-28 17:10:29 -0300

    lalala

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-import
+import express from "express";
+import cors from "cors";
+import { writeFile, readFile } from "node:fs/promises";
+
+const app = express();
+app.use (cors());
+app.use(express.json());
+
+
+// servidor
+
+app.listen(5000, console.log("¡servidor run!"))
+
+
+//funcion de llamado Api
+
+// const getReperotio = async() =>{
+//   const rsResponse = await readFileSync('repertorio.json', 'utf-8');
+//   const data = JSON.parse(rsResponse)
+//   return data;
+// }
