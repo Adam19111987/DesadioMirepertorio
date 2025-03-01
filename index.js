e6dc6decd8d7e3c2b22b4c2cfd3b81c685da3462
AdamDev <a.diaz.1987.08@hotmail.com>
2025-03-01 12:30:20 -0300

    Solucionando tema de id

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(5000, () => {
-  console.log("¡Servidor running en el puerto 3000!");
+  console.log("¡Servidor running en el puerto 5000!");
 });
 
 const getReperotio = async () => {
@@ -22,10 +22,11 @@ const getReperotio = async () => {
 };
 
 app.get("/song", async (req, res) => {
-  try{
+  try {
     const song = await getReperotio();
-  res.json(song)} catch(e){
-   res.status(401).json({message : "Error con solicitud de cancion"})
+    res.json(song);
+  } catch (e) {
+    res.status(401).json({ message: "Error con solicitud de cancion" });
   }
 });
 
@@ -40,27 +41,27 @@ app.get("/song/:id", async (req, res) => {
 });
 
 app.post("/song", async (req, res) => {
- try{
- 
+  try {
     const { id, titulo, artista, tono } = req.body;
-  const song = await getReperotio();
+    const song = await getReperotio();
 
-  const newSong = {
-    id,
-    titulo,
-    artista,
-    tono,
-  };
-  song.push(newSong);
-  await writeFile("repertorio.json", JSON.stringify(song));
-  res.status(201).json(newSong)}
-  catch(e){
-    res.status(401).json({message : "error no se pudo ingresar cancion a repertorio"})}
+    const newSong = {
+      id,
+      titulo,
+      artista,
+      tono,
+    };
+    song.push(newSong);
+    await writeFile("repertorio.json", JSON.stringify(song));
+    res.status(201).json(newSong);
+  } catch (e) {
+    res
+      .status(401)
+      .json({ message: "error no se pudo ingresar cancion a repertorio" });
   }
-);
+});
 
 app.delete("/song/:id", async (req, res) => {
-    try{
   const { id } = req.params;
   let songs = await getReperotio();
   let finder = songs.find((song) => song.id === id);
@@ -70,10 +71,7 @@ app.delete("/song/:id", async (req, res) => {
 
   songs = songs.filter((song) => song.id !== id);
   await writeFile("repertorio.json", JSON.stringify(songs));
-  res.json({ message: "cancion eliminada", songs })
-}catch(e){
-    res.json({ message: "cancion no pudo ser eliminada"})
-}
+  res.json({ message: "cancion eliminada", songs });
 });
 
 app.put("/song/:id", async (req, res) => {
